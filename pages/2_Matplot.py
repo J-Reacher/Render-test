138d084f2dfe1b28b4b57d3a9302f854a15e3e8c
jack <kcajrehcaer@proton.me>
2022-12-14 13:17:52 +0000

    commit

---
+++ pages/2_Matplot.py
@@ -0,0 +1,29 @@
+def main():
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    from utils.hide_st_style import hide
+
+    page_title = 'Matplot'
+    st.set_page_config(page_title=page_title,)
+    st.title(page_title)
+    hide()
+
+    # create 1000 equally spaced points between -10 and 10
+    x = np.linspace(-10, 10, 1000)
+
+    # calculate the y value for each element of the x vector
+    y = (x ** 2 / 9)
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    st.pyplot(fig)
+
+
+if __name__ == '__main__':
+    try:
+        import streamlit as st
+
+        main()
+    except KeyboardInterrupt:
+        st.text('Stopping')
