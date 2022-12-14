3702bd2700da13be52ab25d506101f06bb9d78c4
jack <kcajrehcaer@proton.me>
2022-12-14 05:06:35 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Student Management
+## Ý tưởng cơ bản của dự án:
+- Nhập xuất lưu với file các thông tin của sinh viên
+- Phát triển GUI thích hơp
+- Một Cơ sở dữ liệu thích hợp
+- Đưa lên nền tảng web
---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+
+def print_csv():
+    import pandas as pd
+    _data = './data.csv'
+    st.write(pd.read_csv(_data))
+
+
+def append_csv():
+    import csv
+
+    _append = (int(st.sidebar.number_input('ID: ')),  # 8 attributes
+               st.sidebar.text_input('Name: '),
+               st.sidebar.text_input('Major: '),
+               st.sidebar.date_input('Date of birth (yyyy-mm-dd): '),
+               st.sidebar.text_input('Course (yyyy-yyyy): '),
+               st.sidebar.number_input('Remaining tuition fees: $'),
+               st.sidebar.checkbox('In the dormitory '),
+               st.sidebar.text_input('Address: ')
+               )
+    st.write(_append)
+
+    if to_file := st.sidebar.button('Write to file'):
+        _data = './data.csv'
+        with open(_data, 'a', newline='') as f:
+            csv_writer = csv.writer(f)
+            csv_writer.writerow(_append)
+
+
+def main():
+    from streamlit_option_menu import option_menu as om
+
+    st.title('Student Management')
+    choice = om('Main menu', ['Print', 'Append', ],
+                default_index=0, orientation='horizontal')
+    if choice == 'Print':
+        print_csv()
+    elif choice == 'Append':
+        append_csv()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nStopped')
