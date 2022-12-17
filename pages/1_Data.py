ce26356d1c56682db2180264d6adfa55fd19ddae
jack <kcajrehcaer@proton.me>
2022-12-17 03:55:57 +0000

    add MySQL remote server

--- pages/1_Data.py
+++ pages/1_Data.py
@@ -1,15 +1,20 @@
 import streamlit as st
-from streamlit_option_menu import option_menu as om
+import streamlit.components.v1 as components
+
 import mysql.connector
+import pandas as pd
 
 from utils import Template
 data = Template('Data')
 
+st.sidebar.info(("""
+                    Student management using Python's module [Streamlit](https://streamlit.io/)
+                    and
+                    [MySQL](https://www.mysql.com/) hosted on [FreeSQLdatabase](https://www.freesqldatabase.com/)
+                    """))
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
-
-
 @st.experimental_singleton
 def init_connection():
     return mysql.connector.connect(**st.secrets["mysql"])
@@ -30,55 +35,42 @@ def get_input():
     col1, col2 = st.columns(2)
     with col1:
         column1 = [
-            int(st.number_input('ID: ')),
-            st.text_input('Name: '),
-            st.text_input('Major: '),
-            st.date_input('Date of birth (yyyy-mm-dd): '),
+            int(st.number_input('ID:')),
+            st.text_input('Name:'),
+            st.text_input('Major:'),
+            st.date_input('Date of birth (yyyy-mm-dd):'),
         ]
     with col2:
         column2 = [
-            st.text_input('Course (yyyy-yyyy): '),
+            st.text_input('Course (yyyy-yyyy):'),
             st.number_input('Remaining tuition fee: vnd'),
-            st.checkbox('In the dormitory '),
-            st.text_area('Address: '),
+            st.checkbox('In the dormitory?'),
+            st.text_area('Address:'),
         ]
     return column1 + column2
 
 
-if __name__ == '__main__':
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.markdown("""
-                    Student management using [MySQL](https://www.mysql.com/)
-                    and Python's module [Streamlit](https://streamlit.io/)
-                    """)
-    with col2:
-        st.markdown("""
-                    
-                    """)
-    with col3:
-        run_query('SELECT * FROM Pets')
+def main():
+    def col_names(table_name):
+        return [column[0] for column in run_query(f"SHOW COLUMNS FROM {table_name};")]
 
-    choice = om(None, ['Insert', 'Drop', 'Alter'],
-                default_index=0, orientation='horizontal')
-    
-    # run_query("""
-    #    CREATE TABLE Student (
-    #        Name VARCHAR(64),
-    #        Major VARCHAR(32),
-    #        DOB DATE,
-    #        Course YEAR,
-    #        Remaining_fee FLOAT(10),
-    #        In_dormitory ENUM('y', 'n'),
-    #        Address VARCHAR(128),
-    #        studentID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    #        PRIMARY KEY (studentID)
-    #    );
-    # """)
-    
-    if choice == 'Insert':
-        rows = run_query('SELECT * FROM Pets;')
-        for row in rows:
-            st.write(f"{row[0]} has a :{row[1]}:")
-            
-        # run_query('SELECT * FROM Student;')
+    st.markdown("A connection test with MySQL remote server")
+    with st.echo():
+        st.write('Some queries examples')
+        st.write(pd.DataFrame(run_query('SELECT * FROM Pets;'),  columns=col_names('Pets')))
+        st.write(pd.DataFrame(run_query('SELECT * FROM Student'),columns=col_names('Student')))
+        
+    components.html("""<hr style="height:1px;
+                    border:none;
+                    color:#333;
+                    background-color:#333;" 
+                    /> """)
+
+    # the_table_name = st.text_input('Table name:')
+    the_query = st.text_area('The query:')
+    if st.button('Query'):
+        st.write(pd.DataFrame( run_query(the_query) ))
+
+
+if __name__ == '__main__':
+    main()
---
+++ tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Pets (name varchar(80), pet varchar(80));
+
+INSERT INTO
+    Pets
+VALUES
+    ('Mary', 'dog'),
+    ('John', 'cat'),
+    ('Robert', 'bird');
+
+CREATE TABLE Student (
+    Name VARCHAR(64),
+    Major VARCHAR(32),
+    DOB DATE,
+    Course YEAR,
+    Remaining_fee FLOAT(10),
+    In_dormitory ENUM('y', 'n'),
+    Address VARCHAR(128),
+    studentID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (studentID)
+);
