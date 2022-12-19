a8d1f1cb5969f466850e6b8098034af4aa5102b5
jack <kcajrehcaer@proton.me>
2022-12-19 03:00:26 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,22 @@
 import streamlit as st
 import hydralit as hy
 
-app = hy.HydraApp(title='Simple Muliti_page App', navbar_animation=False,)
+
+app = hy.HydraApp(title='Simple Muliti_page App',)
 
 
 @app.addapp(is_home=True)
 def Home():
-    hy.info('Home')
-    from pages.home import home_page
+    st.title('Home')
+    from Pages.home import home_page
     st.balloons()
     home_page()
 
 
 @app.addapp()
 def Data():
-    from pages.data import sep, execute, example, menu
+    st.title('Data')
+    from Pages.data import sep, execute, example, menu
     execute()
     sep()
 
@@ -29,27 +31,30 @@ def Data():
 
 @app.addapp()
 def Matplot():
-    from pages.matplot import matplot_page
+    st.title('Matplot')
+    from Pages.matplot import matplot_page
     matplot_page()
 
 
 @app.addapp()
 def Gallery():
-    from pages.gallery import gallery_page
+    st.title('Gallery')
+    from Pages.gallery import gallery_page
     st.snow()
     gallery_page()
 
 
 @app.addapp()
 def Contact():
-    from pages.contact import contact_page
+    st.title('Contact')
+    from Pages.contact import contact_page
     contact_page()
 
 
 if __name__ == '__main__':
     # hide_st_style
     # header {visibility: hidden;}
-    # MainMenu {visibility: hidden;}
+    # #MainMenu {visibility: hidden;}
     st.markdown("""
             <style>
             
