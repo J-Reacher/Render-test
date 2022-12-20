da58a4c31f08600a2d5848af481025bb19e33f0e
jack <kcajrehcaer@proton.me>
2022-12-20 04:58:57 +0000

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,9 @@
 # streamlit_app.py
 
-import hydralit as hy
+
 import streamlit as st
-import webbrowser
-
-app = hy.HydraApp(title='Nhat Nam', navbar_animation=False)
 
 
-@app.addapp(title='Home', icon='🏡', is_home=True)
 def home():
     st.title('Home')
     from Pages.home import home_page
@@ -15,11 +11,10 @@ def home():
     home_page()
 
 
-@app.addapp(title='Data')
 def data():
     st.title('Data')
     from Pages.data import sep, execute, example, menu
-    
+
     execute()
     sep()
 
@@ -31,14 +26,12 @@ def data():
     menu()
 
 
-@app.addapp(title='Matplot')
 def matplot():
     st.title('Matplot')
     from Pages.matplot import matplot_page
     matplot_page()
 
 
-@app.addapp(title='Gallery')
 def gallery():
     st.title('Gallery')
     from Pages.gallery import gallery_page
@@ -67,17 +60,14 @@ def sidebar():
 
     # Contact information
     st.sidebar.title('Contact')
-    col1, col2 = st.columns(2)
-    with col1:
-        if st.sidebar.button('Zalo'):
-            webbrowser.open('https://zalo.me/0325808700')
-    with col2:
-        if st.sidebar.button('Facebook'):
-            webbrowser.open(
-                'https://www.facebook.com/profile.php?id=100024994269437')
+    st.markdown("""
+                If you find me interested, contact me on [Zalo](https://zalo.me/0325808700)
+                 of [Facebook](https://www.facebook.com/profile.php?id=100024994269437)
+                """)
     st.sidebar.image('media/ZaloQR.jpg', caption='Zalo me')
 
 
 if __name__ == '__main__':
     sidebar()
-    app.run()
+    if st.button('Home'):
+        st.switch_page('Home')
